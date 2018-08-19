60fc4846675b5cacacd2031485aa2e75c124a01c
yaroslavklymchuk <klm.98yar@gmail.com>
2018-08-19 12:27:35 +0300

    Changes in Runge-Cutta and Adams methods

--- Numerical_methods/Differential equations_Coshie_Task/Runge-Cutta and Adams methods.py
+++ Numerical_methods/Differential equations_Coshie_Task/Runge-Cutta and Adams methods.py
@@ -58,45 +58,48 @@ def adams_4(func, h, x0, y0):
     return Y
 
 
+#x0 = 1, y0 = 1
 #func = lambda x, y: -1 / (y*pow((1-pow(x, 2))/(1-pow(y, 2)), 0.5))
 #y_real = lambda x: pow(4*math.asinh(pow(pow(x-1, 0.5)/pow(2, 0.5), 2)) + 1, 0.5)
 
-func = lambda x, y: (2*pow(x, 2)) / (1+pow(x, 2)) - (2*x*y) / (1+pow(x, 2))
-y_real = lambda x: (2/3 *(1/(pow(x, 2) +1))) * (1+pow(x,3))
+func = lambda x, y: (3*y - x - 4) / (3*x + 3)
+y_real = lambda x: 1 - (x+1)*math.log(math.fabs(x+1)) / 3
+
 
 X = get_X(0, 0.1)
 answers = [y_real(i) for i in X]
 
-H = np.linspace(0.001, 0.1, 20)
-predictions_runge_cutta = [runge_cutta_4(func, h, 0, 2/3) for h in H]
-errors_runge_cutta = error(answers, runge_cutta_4(func, 0.1, 0, 2/3))
+H = np.linspace(0.001, 0.1, 10)
+predictions_runge_cutta = [runge_cutta_4(func, h, 0, 1) for h in H]
+errors_runge_cutta = error(answers, runge_cutta_4(func, 0.1, 0, 1))
 errors_runge_cutta_h = [error_([y_real(i) for i in get_X(0, h)],
                                predictions_runge_cutta[i], h) for i, h in enumerate(H)]
 
 
 
-predictions_adams = [adams_4(func, h, 0, 2/3) for h in H]
-errors_adams = error(answers, adams_4(func, 0.1, 0, 2/3))
+predictions_adams = [adams_4(func, h, 0, 1) for h in H]
+errors_adams = error(answers, adams_4(func, 0.1, 0, 1))
 errors_adams_h = [error_([y_real(i) for i in get_X(0, h)],
                                predictions_adams[i], h) for i, h in enumerate(H)]
 
 
 frame = pd.DataFrame(columns=['Y_real', 'Runge_Cutt', 'Adams', 'Errors_Runge_Cutta', 'Errors_Adams'])
 frame['Y_real'] = pd.Series(answers)
-frame['Runge_Cutt'] = pd.Series(runge_cutta_4(func, 0.1, 0, 2/3))
-frame['Adams'] = pd.Series(adams_4(func, 0.1, 0, 2/3))
+frame['Runge_Cutt'] = pd.Series(runge_cutta_4(func, 0.1, 0, 1))
+frame['Adams'] = pd.Series(adams_4(func, 0.1, 0, 1))
 frame['Errors_Runge_Cutta'] = pd.Series(errors_runge_cutta)
 frame['Errors_Adams'] = pd.Series(errors_adams)
 frame.to_csv('results.csv', index = False)
 
 plt.title('Решение (h=0.1)')
-plt.plot(X, answers, 'r', X, runge_cutta_4(func, 0.1, 0, 2/3), 'b', X, adams_4(func, 0.1, 0, 2/3), 'y')
+plt.plot(X, answers, 'r', X, runge_cutta_4(func, 0.1, 0, 1), 'b', X, adams_4(func, 0.1, 0, 1), 'y')
 plt.legend(['Точное решение', 'Метод Рунге-Кутты', 'Метод Адамса'])
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.show()
 plt.title('График зависимости ошибок от h')
-plt.plot(H, errors_runge_cutta_h, 'y', H, errors_adams_h)
+plt.plot(H, errors_runge_cutta_h, 'y', H, errors_adams_h, 'b')
+plt.legend(['Ошибка метода Рунге-Кутта', 'Ошибка метода Адамса'])
 plt.xlabel('H')
 plt.ylabel('Ошибка')
 plt.show()
